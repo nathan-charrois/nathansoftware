9a9eb45f35a6decc67c5d76c8add01a70f4fc219
Nathan Charrois <ncharrois@quoter.com>
2025-09-10 21:32:21 -0700

    npm build / start

--- server/app.ts
+++ server/app.ts
@@ -2,7 +2,6 @@ import cors from 'cors'
 import express from 'express'
 import fs from 'fs'
 import path from 'path'
-import { fileURLToPath } from 'url'
 
 import { handleGetPreferences, handlePostPreferences } from './models/preferences'
 
@@ -13,9 +12,6 @@ app.use(express.json())
 
 app.get('/preferences', handleGetPreferences)
 app.post('/preferences', handlePostPreferences)
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 const candidates = [
   path.resolve(process.cwd(), '..', 'build', 'client'),
